17c903535c4869c6c6d4a8296c8705b0823e2b3a
mechPenSketch <pikadon92@gmail.com>
2023-03-14 17:41:24 +0800

    Bring Forward to Godot 4.0

--- addons/jrpg_overwolrd/dialog.gd
+++
@@ -1,8 +0,0 @@
-tool
-extends ConfirmationDialog
-
-export(String, DIR) var dir
-
-func _on_item_selected(i):
-	#find_node("HSprite").set_visible(i in [1, 2])
-	pass
---
+++ overworld/playing_pieces/playing_piece.gd
@@ -0,0 +1,4 @@
+@icon("playing_piece.svg")
+extends Sprite2D
+
+@export var timeline: String
