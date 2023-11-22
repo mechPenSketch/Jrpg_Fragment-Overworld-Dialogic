0b697779e6ac3010eabf5af03cc2bc8dff811ddb
mechPenSketch <pikadon92@gmail.com>
2023-11-22 15:53:33 +0800

    Implemented Recurring Translations

--- content/overworld/global.gd
+++ content/overworld/global.gd
@@ -17,6 +17,23 @@ func _ready():
 	var pksc_player = load(FP_PLAYER)
 	player = pksc_player.instantiate()
 	add_child(player)
+	
+	## Would fail to get Dialogic.VAR if loaded before
+	## Dialogic (see Autoload for poistioning of
+	## singletons).
+	## To prevent this, call when its parent is ready.
+	get_parent().ready.connect(_on_parent_ready)
+
+
+func _on_parent_ready():
+	tr_recurring_choices()
+	
+	get_parent().ready.disconnect(_on_parent_ready)
+
+
+func tr_recurring_choices():
+	for var_path in Dialogic.VAR.RecurringChoices.variables(true):
+		Dialogic.VAR.set_variable(var_path, tr(var_path))
 
 
 ## Changes map by replacing the scene.
